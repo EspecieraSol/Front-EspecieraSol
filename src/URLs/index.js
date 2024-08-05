5baaad7afbded003bd7e6bfc21a838af3199f661
fmarcos38 <fmarcos_23@hotmail.com>
2024-08-05 11:10:58 -0300

    remito

--- src/URLs/index.js
+++ src/URLs/index.js
@@ -1,5 +1,6 @@
 //urls Backend
-const local = "http://localhost:3001";
-const deploy = "https://back-especiera.onrender.com";
+//const local = "http://localhost:3001";
+const deploy = "https://back-especiera.onrender.com"; //render
+//const deploy = "back-prueba-vercel.vercel.app"; //vercel 
 
 export const actual = deploy;
