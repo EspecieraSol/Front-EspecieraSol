f905fa854d880ea477ba4c269d6970a2200e6e43
fmarcos38 <fmarcos_23@hotmail.com>
2024-08-07 10:09:31 -0300

    url google

--- src/URLs/index.js
+++ src/URLs/index.js
@@ -2,4 +2,5 @@
 const local = "http://localhost:3001";
 const render = "https://back-especiera.onrender.com"; //render
 const google = "https://evocative-ethos-431622-q4.rj.r.appspot.com"; //google cloud
-export const actual = render;
+
+export const actual = google;
