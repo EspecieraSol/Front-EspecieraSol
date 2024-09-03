35be94472597d608976ed78c9b2fde720c176818
fmarcos38 <fmarcos_23@hotmail.com>
2024-09-03 14:53:15 -0300

    quito reload

--- src/Componentes/Remito/index.jsx
+++ src/Componentes/Remito/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import logoRemito from '../../Imagenes/logoYtexto.jpg';
 import { useDispatch, useSelector } from 'react-redux';
-import { creaRemito, getRemitosCliente, resetCliente } from '../../Redux/Actions';
+import { creaRemito, getAllRemitos, getRemitosCliente, resetCliente } from '../../Redux/Actions';
 import { formatDate, formatMoney, cortaPalabra } from '../../Helpers';
 import Swal from 'sweetalert2';
 import './estilos.css';
@@ -103,9 +103,9 @@ function Remito({ operacion, fecha, numUltimoRemito, cliente, clienteExiste, ite
             Swal.fire({
                 title: 'Creado con exito !!',
                 icon: 'success'
-            }, navigate('/listaRemitosVentas'));            
-            //window.location.reload()
-            
+            }, navigate('/listaRemitosVentas')); 
+            dispatch(getAllRemitos());           
+            //window.location.reload()            
         }
     };
     //función crea las filas de la tabla 8 y llena las q sean necesarias
