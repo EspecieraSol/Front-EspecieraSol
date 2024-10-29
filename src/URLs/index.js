53d55a2249dfb5b2fdbb2ff794d43460a2348e09
fmarcos38 <fmarcos_23@hotmail.com>
2024-10-29 14:44:25 -0300

    url

--- src/URLs/index.js
+++ src/URLs/index.js
@@ -2,4 +2,4 @@
 const local = "http://localhost:3001";
 const google = "https://evocative-ethos-431622-q4.rj.r.appspot.com"; //google cloud
 
-export const actual = local;
+export const actual = google;
