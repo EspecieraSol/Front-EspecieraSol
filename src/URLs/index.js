601464b70ec9f620e1e52a34216860af162ae5ae
fmarcos38 <fmarcos_23@hotmail.com>
2024-11-22 15:55:12 -0300

    reportes

--- src/URLs/index.js
+++ src/URLs/index.js
@@ -1,7 +1,6 @@
 //urls Backend
 //const local = "http://localhost:3001";
-//const google = "https://evocative-ethos-431622-q4.rj.r.appspot.com"; //google cloud
-//heroku
-const heroku = "https://especiera-86aaad050caf.herokuapp.com";
+const google = "https://evocative-ethos-431622-q4.rj.r.appspot.com"; //google cloud
 
-export const actual = heroku;
+
+export const actual = google;
