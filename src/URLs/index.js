d6c4fffe87bf715d48fc344541618c7490df60b1
fmarcos38 <fmarcos_23@hotmail.com>
2025-01-31 11:23:22 -0300

    prod

--- src/URLs/index.js
+++ src/URLs/index.js
@@ -1,6 +1,6 @@
 //urls Backend
-const local = "http://localhost:3001";
+//const local = "http://localhost:3001";
 //heroku
-//const heroku = "https://especiera-22f54012404d.herokuapp.com";
+const heroku = "https://especiera-22f54012404d.herokuapp.com";
 
-export const actual = local;
+export const actual = heroku;
